b38788e2925abf4685c2ef1df6cc94c67408a95a
bgujjala <bgujjala@deloitte.com>
2021-09-16 15:04:16 +0530

    merging two lists

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+dict1 = {'ten': 10,'twenty': 20, 'thirty': 30}
+dict2 = {'thirty': 30, 'fourty': 40, 'fifty': 50}
+dict3 = {**dict1, **dict2}
+print(dict3)
